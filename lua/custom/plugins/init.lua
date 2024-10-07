9aa1ab0ed3b1cfecf2af999c8fc28935f1101076
Vikas Rangarajan <vikas_rangarajan@cable.comcast.com>
2024-10-07 18:27:09 -0500

    feat: My custom neovim config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,13 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+vim.wo.foldmethod = "expr"
+vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
+vim.wo.foldenable = true
+vim.o.autochdir = true
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.softtabstop = 2
+
 return {}
